27fc5632b0ec574939a0c9d775aa2772e7a8b551
balapavithra <pavikannan2991@gmail.com>
2026-02-16 15:59:23 +0530

    Small change

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./page";
 import { Task1 } from "./page/task-1";
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
@@ -14,5 +14,3 @@ const App: React.FC = () => {
     </BrowserRouter>
   );
 };
-
-export default App;
